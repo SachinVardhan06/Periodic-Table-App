244c3669849a31414ec2684aff3331f1cb2f0a7e
Sachin Vardhan <106000009+SachinVardhan06@users.noreply.github.com>
2024-08-26 10:10:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,44 @@
 let username = document.querySelector("#user-name");
 let useremail = document.querySelector("#user-email");
-let userphoneno = document.querySelector("user-phoneno");
-let usreaddress = document.querySelector("user-address");
-let submitbutton = document.querySelector("#palce-button");
+let userphoneno = document.querySelector("#user-phoneno");
+let useraddress = document.querySelector("#user-address");
+let submitbutton = document.querySelector("#place-button");
+let orderPlacedDiv = document.querySelector("#order-placed");
 
-let aboutpage = document.querySelector("#about-page");
-
-let buttonbuy = document.querySelector(".button-buy1");
-
-// ----------------------- Details ------------------
-
+// Script URL for Google Sheets
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbwufPUX44o5FisSg50c37atADOP7dOWNRu8QB-8BIVZVDNbq4AWQOh1tR8iVOMzX4ojiA/exec";
 const form = document.forms["submit-to-google-sheet"];
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
+
+  // Validate fields
+  var name = username.value.trim();
+  var email = useremail.value.trim();
+  var phone = userphoneno.value.trim();
+  var address = useraddress.value.trim();
+
+  if (!name || !email || !phone || !address) {
+    alert("Input fields cannot be empty.");
+    return;
+  }
+
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
     .then((response) => {
       console.log("Success!", response);
-      location.reload(); 
+
+      document.getElementById("buy-text").style.display = "none";
+      document.querySelectorAll(".form-data").forEach(function (element) {
+        element.style.display = "none";
+      });
+      submitbutton.style.display = "none";
+
+      orderPlacedDiv.style.display = "block";
+
+      setTimeout(() => {
+        window.location.href = "index.html";
+      }, 5000);
     })
     .catch((error) => console.error("Error!", error.message));
 });
-
-// -------------------- Oredr Placed -----------------------
-
-document
-  .getElementById("place-button")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-
-    // Get input fields
-    var name = document.getElementById("user-name").value.trim();
-    var email = document.getElementById("user-email").value.trim();
-    var phone = document.getElementById("user-phoneno").value.trim();
-    var address = document.getElementById("user-address").value.trim();
-
-  
-    if (!name || !email || !phone || !address) {
-      alert("Input fields cannot be empty.");
-      return;
-    }
-
-    document.getElementById("buy-text").style.display = "none";
-    document.querySelectorAll(".form-data").forEach(function (element) {
-      element.style.display = "none";
-    });
-    document.getElementById("place-button").style.display = "none";
-
-  
-    document.getElementById("order-placed").style.display = "block";
-
-    setTimeout(() => {
-      window.location.href = "index.html";
-    }, 5000);
-  });
